5d5919a8241d4933581c7c9014081b8e66008fc5
abhipandey11 <abhi.its2004bhopal@gmail.com>
2026-05-30 21:44:48 +0530

    Add requests dependency

--- services/comment-service/app/utils/blog_client.py
+++ services/comment-service/app/utils/blog_client.py
@@ -1,10 +1,19 @@
+import os
 import requests
+
+BLOG_SERVICE_URL = os.getenv("BLOG_SERVICE_URL")
 
 
 def blog_exists(blog_id):
 
-    response = requests.get(
-        f"http://localhost:8002/blogs/{blog_id}"
-    )
+    try:
 
-    return response.status_code == 200
+        response = requests.get(
+            f"{BLOG_SERVICE_URL}/posts/{blog_id}",
+            timeout=5
+        )
+
+        return response.status_code == 200
+
+    except requests.RequestException:
+        return False
