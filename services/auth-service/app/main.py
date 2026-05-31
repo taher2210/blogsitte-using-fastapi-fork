8b9e514704d034ca60951827e6be0caebae483ca
Taher Patanwala <45094466+taher2210@users.noreply.github.com>
2026-05-31 14:20:08 +0530

    Update main.py

--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.auth_routes import router
 from app.db.database import Base, engine
 
@@ -6,9 +8,19 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 
 @app.get("/")
 def home():
-    return {"message": "Auth Service Running"}
+    return {
+        "message": "Auth Service Running"
+    }
